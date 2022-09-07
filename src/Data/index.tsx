710e5fa4f2d5307d3fec38a87e412b8f8aaad892
sanskar sahu <sahup3296@gmail.com>
2022-09-07 02:47:52 +0000

    Update 2 files

---
+++ src/Data/index.tsx
@@ -0,0 +1,24 @@
+import { v4 } from 'uuid';
+
+export default [
+    {
+        id: v4(),
+        text: 'Get a Problem',
+        isTodoDone: false,
+    },
+    {
+        id: v4(),
+        text: 'Feel along',
+        isTodoDone: true,
+    },
+    {
+        id: v4(),
+        text: 'Just call me',
+        isTodoDone: true,
+    },
+    {
+        id: v4(),
+        text: 'On a telephone',
+        isTodoDone: false,
+    },
+];
